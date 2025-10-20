902db4638a73e79eef209dac190d19fd802e4692
Zen-Code-21 <zencode731@gmail.com>
2025-10-20 15:53:26 +0530

    06-operators

--- 06-operators/operators_logical.py
+++ 06-operators/operators_logical.py
@@ -7,15 +7,15 @@
 #or
 #not
 
-# print(True and False)
-# print(True and True)
-# print(False and True)
-# print(False and False)
+print(True and False)
+print(True and True)
+print(False and True)
+print(False and False)
 
-# print(True or False)
-# print(True or True)
-# print(False or True)
-# print(False or False)
+print(True or False)
+print(True or True)
+print(False or True)
+print(False or False)
 
 
 # print(not(False))
