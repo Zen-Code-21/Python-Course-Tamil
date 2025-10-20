b4d65548d3a1df12aac42eb3a8b8abba4b132008
Zen-Code-21 <zencode731@gmail.com>
2025-10-20 20:36:23 +0530

    07-conditionals

--- 07-conditionals/if-elif-else.py
+++ 07-conditionals/if-elif-else.py
@@ -1,11 +1,11 @@
 
 
 
-# age = 18
+age = 18
 
-# if(age > 18):
-#     print("yes you can drive")
-# elif(age == 18):
-#     print("you have to write an exam in driving school")
-# else:
-#     print("no you cannot drive")
+if(age > 18):
+    print("yes you can drive")
+elif(age == 18):
+    print("you have to write an exam in driving school")
+else:
+    print("no you cannot drive")
